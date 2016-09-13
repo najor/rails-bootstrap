39f950fcf64cb152eb3b640101840047350f0ced
najorcruzcruz <najorcruzcruz@gmail.com>
2016-09-13 10:06:05 +0100

    Logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
 
   def login
-    redirect_to 'home' if session[:user_id]
+    redirect_to controller: 'home' if session[:user_id]
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:message] = I18n.t('logout_successful')
+    redirect_to '/'
   end
 
   def login_attempt
     customer = Customer.find_by_email(params[:email])
     if customer && customer.authenticate(params[:pass])
       session[:user_id] = customer.id
-      redirect_to '/sessions/home'
+      redirect_to controller: 'home'
     else
       flash[:error] = customer ? I18n.t('login_password_incorret') : I18n.t('login_user_not_found')
       redirect_to '/'
     end
   end
 
-  def home
-  end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,14 +9,19 @@ class SessionsControllerTest < ActionController::TestCase
 
   def test_login_attempt_no_params
     post :login_attempt
-    assert_redirected_to 'login'
+    assert_redirected_to action: 'login'
     assert_equal I18n.t('login_user_not_found'), flash[:error]
+  end
+
+  def test_logout
+    get :logout
+    assert_redirected_to '/'
+    assert_equal I18n.t('logout_successful'), flash[:message]
   end
 
   def test_login_attempt_params
     post :login_attempt, {email: 'joseph@gmail.com', pass: '987654321'}
-    assert_redirected_to 'home'
-
+    assert_redirected_to controller: 'home'
   end
 
 end
